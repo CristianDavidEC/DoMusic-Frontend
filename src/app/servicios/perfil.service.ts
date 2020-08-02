5717a8076dc3c90f656ecf39b30bdfb8ebd9003e
Luis David <luis-david45g@hotmail.com>
2020-08-02 17:03:44 -0500

    perfiles inicio con dudas

---
+++ src/app/servicios/perfil.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import{PerfilModel} from '../modelos/perfil.model';
+import {HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PerfilService {
+
+  constructor(
+    // private http: HttpClient
+  ) { }
+
+  /* CrearPerfil(model: PerfilModel){
+    this.http.post('http://localhost:3000/musico-profesionals', )
+
+  } */
+
+}
