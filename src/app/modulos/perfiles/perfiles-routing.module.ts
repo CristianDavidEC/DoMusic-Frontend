85c534f0df7eabc01f3a2751e628774209021617
Cristian David Escobar Castillo <cristianescobarcdec@gmail.com>
2020-08-03 09:31:51 -0500

    Diseño

--- src/app/modulos/perfiles/perfiles-routing.module.ts
+++ src/app/modulos/perfiles/perfiles-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TipoPerfilComponent} from './tipo-perfil/tipo-perfil.component'
-
+import { TipoPerfilComponent } from './tipo-perfil/tipo-perfil.component'
+import { CrearPerfilComponent } from './crear-perfil/crear-perfil.component'
 
 const routes: Routes = [
   {
     path: 'tipo-perfil',
     component: TipoPerfilComponent
-  }
-
-  
+  },
+  {
+    path: 'crear-perfil',
+    component: CrearPerfilComponent
+  }  
 ];
 
 @NgModule({
