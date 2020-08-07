fc6e28d0468fc1b22c7a728dcc0003f5e6295c69
Luis David <luis-david45g@hotmail.com>
2020-08-06 20:27:48 -0500

    paginacion y spinner

--- src/app/modulos/parametros/publicaciones/mostrar-publicacion/mostrar-publicacion.component.ts
+++ src/app/modulos/parametros/publicaciones/mostrar-publicacion/mostrar-publicacion.component.ts
@@ -3,6 +3,8 @@ import { PublicacionesService} from '../../../../servicios/parametros/publicacio
 import { PublicacionModel } from 'src/app/modelos/parametros/publicacion.model';
 import { FormsConfig } from 'src/app/config/forms-config';
 
+import { NgxSpinnerModule } from "ngx-spinner";
+import { NgxSpinnerService } from "ngx-spinner";
 
 declare const ShowNotificationMessage: any;
 declare const ShowRemoveConfimationPublic: any;
@@ -14,20 +16,32 @@ declare const ShowRemoveConfimationPublic: any;
 })
 export class MostrarPublicacionComponent implements OnInit {
 
+  pagina: number = 1;
   recordList : PublicacionModel[];
   eliminarPubliId: String ='';
+  publiPorPagina: number = FormsConfig.ELEMENTOS_PAGINA;
 
   constructor(
-    private service: PublicacionesService
+    private service: PublicacionesService,
+    private spinner: NgxSpinnerService
   ) { }
 
   ngOnInit(): void {
+
+    this.spinner.show();
+    
+
     this.getRecordsList()
+
+
   }
 
   getRecordsList(){
     this.service.getAllRecords().subscribe(records => {
       this.recordList = records;
+      setTimeout(() => {
+        this.spinner.hide();
+      },1000)
     },
     error => {ShowNotificationMessage ("Hubo un problema con la comunicación en el Backend")})
   }
