a4b8292d40e778d253c9ee04d64a86114d4acc0c
Tatianagh <carol.1701613339@ucaldas.edu.co>
2020-08-06 21:59:21 -0500

    creación de publicaciones

--- src/app/modulos/parametros/publicaciones/crear-publicacion/crear-publicacion.component.ts
+++ src/app/modulos/parametros/publicaciones/crear-publicacion/crear-publicacion.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PerfilService } from 'src/app/servicios/perfil.service';
+import { PublicacionesService } from 'src/app/servicios/parametros/publicaciones.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PublicacionModel } from 'src/app/modelos/parametros/publicacion.model';
+
+declare const ShowNotificationMessage:any;
 
 @Component({
   selector: 'app-crear-publicacion',
@@ -7,9 +14,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearPublicacionComponent implements OnInit {
 
-  constructor() { }
+  fgValidator: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private servicio: PublicacionesService,
+    private route: ActivatedRoute,
+    private router: Router
+
+  ) { }
 
   ngOnInit(): void {
+    this.FormBuilding();
+  }
+
+  FormBuilding(){ 
+    this.fgValidator = this.fb.group({
+      titulo: ['', [Validators.required, Validators.minLength(2)]],
+      contenido: ['', [Validators.required, Validators.minLength(2)]],
+      fecha: ['', [Validators.required]],
+    });
+  }
+
+  crearPublicacion(){
+    if(this.fgValidator.invalid){
+      ShowNotificationMessage('Formulario inválido')
+    }else{
+        let model=this.getPubliDatos();
+        this.servicio.guardarNuevoRegistro(model).subscribe(data => {
+          console.log(data);
+          if(data){
+            ShowNotificationMessage('Registro exitoso');
+            this.router.navigate(['/parametros/publicaciones']);
+          }
+          else{
+            ShowNotificationMessage('Error!');
+          }
+        });      
+    }
+  }
+
+  get fgv(){
+    return this.fgValidator.controls;
+  }
+
+
+  getPubliDatos(): PublicacionModel{
+    let model = new PublicacionModel();
+    model.titulo = this.fgv.titulo.value;
+    model.contenido = this.fgv.contenido.value;
+    model.fecha = (this.fgv.fecha.value);
+    
+
+    return model;
   }
 
 }
