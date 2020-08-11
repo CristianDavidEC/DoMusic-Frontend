59178e3ff3aa842f813d9fa1fb8014db37f6ccd8
Cristian David Escobar Castillo <cristianescobarcdec@gmail.com>
2020-08-11 00:20:42 -0500

    archivos nuevos

---
+++ src/app/modulos/perfiles/vista-perfil/vista-perfil.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VistaPerfilRoutingModule } from './vista-perfil-routing.module';
+import { VistaPerfilComponent } from './vista-perfil.component';
+
+
+@NgModule({
+  declarations: [VistaPerfilComponent],
+  imports: [
+    CommonModule,
+    VistaPerfilRoutingModule
+  ]
+})
+export class VistaPerfilModule { }
